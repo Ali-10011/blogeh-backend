0c37dd03efcbd75023fd903d09e8be1a97b087e7
Ali-10011 <98711622+Ali-10011@users.noreply.github.com>
2023-04-30 22:29:05 +0500

    Set up base app with database

---
+++ server.js
@@ -0,0 +1,21 @@
+const { json } = require("express");
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    //Your Mongo DB Connection String here.
+  .then((data) => {
+    console.log("Connected");
+    app.listen(process.env.PORT || 5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//MiddleWare
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
